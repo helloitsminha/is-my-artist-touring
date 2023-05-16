f39a5445f78931a924bd6b977630c1dd2d673bba
helloitsminha <minha.mh.lee@gmail.com>
2023-05-16 17:18:33 -0400

    first successful api call

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
-import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
+import { useEffect } from 'react';
 
 function App() {
-  return (
-    <div className="App">
- 
-    </div>
-  );
+  
+    useEffect(() => {
+      axios ({
+        url: 'https://api.spoonacular.com/recipes/findByIngredients',
+        method: 'GET',
+        dataResponse: 'json',
+        params: {
+          apiKey: '434692a1f35a4107addf9b830c5d7a39',
+          ingredients: 'tomato,eggs,cheese',
+          number: 20,
+          ranking: 1
+        },
+      }).then((response) => {
+        console.log(response);
+      })
+    }, []);
 }
 
 export default App;
